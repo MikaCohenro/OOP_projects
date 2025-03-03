830bc981df5da2d0a3b0c3e19fc4fed06828b9de
MikaCohenro <mika.cohenro@gmail.com>
2025-03-03 10:14:18 +0200

    Delete EX1 directory

--- EX1/src/Mark.java
+++
@@ -1,7 +0,0 @@
-/**
- * Enumeration representing marks on the game board.
- * Possible values: BLANK, X, O.
- */
-public enum Mark {
-    BLANK, X, O
-}
--- EX1/src/Player.java
+++
@@ -1,7 +0,0 @@
-/**
- * interface for a player that plays the game
- */
-interface Player {
-
-    void playTurn(Board board, Mark mark);
-}
